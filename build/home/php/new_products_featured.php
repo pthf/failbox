2edbd725443c07974a9f385d87c80c407cb531d5
Guillermo <memo.9321@gmail.com>
2016-09-14 13:45:24 -0500

    grids destacatos/descuentos

--- build/home/php/new_products_featured.php
+++ build/home/php/new_products_featured.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../admin/db/conexion.php");
 
-$query = "SELECT * FROM Productos p INNER JOIN Marcas m ON m.IdMarca = p.Marcas_IdMarca WHERE Destacado = 'SI' AND Estatus = 'Activo' AND Stock > 0";
+$query = "SELECT * FROM Productos p INNER JOIN Marcas m ON m.IdMarca = p.Marcas_IdMarca WHERE Destacado = 'SI' AND Estatus = 'Activo' AND Stock > 0 ORDER BY p.PrecioFailbox DESC LIMIT 20";
 $resultado = mysql_query($query,Conectar::con()) or die(mysql_error());
 
 $productos = array();
